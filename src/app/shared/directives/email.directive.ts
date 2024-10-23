a60deadb88591190d253c1679508f2135b113921
lbmrytskv <liubomyryatskiv97@gmail.com>
2024-10-23 15:03:13 +0200

    fixed errors

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,21 +1,21 @@
-import { Directive } from "@angular/core";
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Directive({
-    selector: '[emailValidatorDirective]',
-    providers: [
-      {
-        provide: NG_VALIDATORS,
-        useExisting: EmailValidatorDirective,
-        multi: true
-      }
-    ]
-  })
-  export class EmailValidatorDirective implements Validator {
-    validate(control: AbstractControl): ValidationErrors | null {
-      const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-      const valid = emailRegex.test(control.value);
-      return valid ? null : { invalidEmail: true };
+  selector: '[emailValidatorDirective]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailValidatorDirective,
+      multi: true
     }
+  ]
+})
+export class EmailValidatorDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const valid = emailRegex.test(control.value);
+    return valid ? null : { invalidEmail: true };
   }
-
+}
 
